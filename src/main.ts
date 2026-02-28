5c3678193bdc2f8a28c0be9597a8a43bc9875f33
[dyad] <git@dyad.sh>
2026-02-28 01:05:26 -0800

    [dyad] Switching PrimeVue theme to Aura preset (no CSS import) - wrote 1 file(s)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { createApp } from "vue";
 import PrimeVue from "primevue/config";
 import ToastService from "primevue/toastservice";
 
-import "@primeuix/themes/aura/theme.css";
+import Aura from "@primevue/themes/aura";
 
 import "./styles.css";
 import "primeicons/primeicons.css";
@@ -13,6 +13,9 @@ import App from "./App.vue";
 createApp(App)
   .use(PrimeVue, {
     ripple: true,
+    theme: {
+      preset: Aura,
+    },
   })
   .use(ToastService)
   .mount("#app");
